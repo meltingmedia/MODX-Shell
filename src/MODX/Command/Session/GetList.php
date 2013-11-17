97de0685863b98a0aaead78f255a60b029cd7f97
Romain <romain@melting-media.com>
2013-11-17 14:21:12 +0100

    Playing with sessions

---
+++ src/MODX/Command/Session/GetList.php
@@ -0,0 +1,64 @@
+<?php namespace MODX\Command\Session;
+
+use MODX\Command\ProcessorCmd;
+
+class GetList extends ProcessorCmd
+{
+    protected $headers = array(
+        'id', 'access', 'user'
+    );
+
+    protected $name = 'session:list';
+    protected $description = 'List existing modSession';
+
+    protected function process()
+    {
+        //$this->handleColumns();
+
+        /** @var \Symfony\Component\Console\Helper\TableHelper $table */
+        $table = $this->getApplication()->getHelperSet()->get('table');
+        $table->setHeaders($this->headers);
+
+
+        $c = $this->modx->newQuery('modSession');
+
+        $collection = $this->modx->getIterator('modSession', $c);
+
+        //$rows = array();
+        /** @var \modDbRegisterTopic $object */
+        foreach ($collection as $object) {
+            $row = $object->toArray();
+            $row['user'] = $this->getUser($row['id']);
+//            if ($row['data'] != '') {
+//                $row['a'] = unserialize($row['data']);
+//            }
+//            print_r($row);
+
+            $table->addRow($this->processRow($row));
+        }
+
+        $table->render($this->output);
+    }
+
+    protected function getUser($session)
+    {
+        $value = 'Anonymous';
+
+        $c = $this->modx->newQuery('modUserProfile');
+        $c->where(array(
+            'sessionid' => $session,
+        ));
+
+        if ($this->modx->getCount('modUserProfile', $c) > 0) {
+            $c->limit(1);
+            /** @var \modUserProfile $profile */
+            $profile = $this->modx->getObject('modUserProfile', $c);
+            /** @var \modUser $user */
+            $user = $profile->getOne('User');
+
+            $value = $user->get('username');
+        }
+
+        return $value;
+    }
+}
---
+++ src/MODX/Command/Session/Remove.php
@@ -0,0 +1,53 @@
+<?php namespace MODX\Command\Session;
+
+use MODX\Command\ProcessorCmd;
+use Symfony\Component\Console\Input\InputArgument;
+
+class Remove extends ProcessorCmd
+{
+    protected $name = 'session:remove';
+    protected $description = 'Remove given modSession';
+
+    protected function getArguments()
+    {
+        return array(
+            array(
+                'id',
+                InputArgument::REQUIRED,
+                'The session id'
+            ),
+        );
+    }
+
+    protected function process()
+    {
+        /** @var \modSession $session */
+        $session = $this->modx->getObject('modSession', $this->argument('id'));
+        if (!$session) {
+            return $this->error('Session not found');
+        }
+
+        if (!$session->remove()) {
+            return $this->error('Something went wrong while trying to delete the session');
+        }
+
+        $this->removeSessionIDS();
+
+        $this->info('Session removed');
+    }
+
+    protected function removeSessionIDS()
+    {
+        $c = $this->modx->newQuery('modUserProfile');
+        $c->where(array(
+            'sessionid:!=' => '',
+        ));
+
+        $collection = $this->modx->getIterator('modUserProfile', $c);
+        /** @var \modUserProfile $object */
+        foreach ($collection as $object) {
+            $object->set('sessionid', '');
+            $object->save();
+        }
+    }
+}
