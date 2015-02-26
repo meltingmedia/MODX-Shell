7e471a6484c32e063cc930402ea88e6e810eb449
Romain <romain@melting-media.com>
2015-02-26 17:38:27 +0100

    Simplification & cleanup of instances configuration listing command

--- src/Command/Config/GetList.php
+++ src/Command/Config/GetList.php
@@ -16,9 +16,8 @@ class GetList extends BaseCmd
         $config = $this->getApplication()->instances->getAll();
 
         if (empty($config)) {
-            return $this->error('No configuration file found');
+            return $this->error('No entries found');
         }
-        $currentDir = $this->getApplication()->getCwd();
 
         $table = new Table($this->output);
         //$table->setStyle('compact');
@@ -26,21 +25,18 @@ class GetList extends BaseCmd
             'Name', 'Path', 'Revo version'
         ));
         $idx = 1;
+        $current = $this->getApplication()->instances->current();
 
         foreach ($config as $name => $data) {
             $version = '<error>Unknown</error>';
             $color = '';
-            if ($this->isCurrent($currentDir, $data)) {
-                $color = 'info';
-                $modx = $this->getMODX();
-                if ($modx) {
-                    $versionData = $modx->getVersionData();
-                    $version = $versionData['full_version'];
+            if (isset($data['core_path'])) {
+                $version = $this->getRemoteMODXVersion($data['core_path']);
+                if ($name === $current) {
+                    $color = 'info';
                 }
             } elseif (!file_exists($data['base_path']) || !file_exists($data['base_path'] . 'config.core.php')) {
                 $color = 'error';
-            } elseif (isset($data['core_path'])) {
-                $version = $this->getRemoteMODXVersion($data['core_path']);
             }
 
             $row = array(
@@ -89,38 +85,5 @@ class GetList extends BaseCmd
         $v = include $file;
 
         return $v['full_version'];
-    }
-
-    /**
-     * Check whether or not we are in declared MODX installation
-     *
-     * @param string $currentDir
-     * @param array $data
-     *
-     * @return bool
-     */
-    protected function isCurrent($currentDir, array $data)
-    {
-        $current = false;
-        $length = strlen($data['base_path']);
-        $result = substr($currentDir, 0, $length);
-
-        if ($result == $data['base_path']) {
-            $current = true;
-        }
-
-        return $current;
-    }
-
-    /**
-     *
-     * @param int $number
-     * @param int $digits
-     *
-     * @return string
-     */
-    protected function formatNumber($number, $digits)
-    {
-        return str_pad($number, $digits, '0', STR_PAD_LEFT);
     }
 }
